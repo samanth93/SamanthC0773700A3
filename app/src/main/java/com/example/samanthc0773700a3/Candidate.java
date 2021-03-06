d031051948ebe56fe4a2342aa47acd818e2b49c5
samanth93 <samanth.sid@gmail.com>
2021-03-05 20:37:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/samanthc0773700a3/Candidate.java
@@ -0,0 +1,24 @@
+package com.example.samanthc0773700a3;
+import java.io.Serializable;
+
+public class Candidate implements Serializable {
+    public String getCanName() {
+        return canName;
+    }
+
+    public void setCanName(String canName) {
+        this.canName = canName;
+    }
+
+    private String canName;
+
+    public int getNumOfVotes() {
+        return numOfVotes;
+    }
+
+    public void setNumOfVotes(int numOfVotes) {
+        this.numOfVotes = numOfVotes;
+    }
+
+    private int numOfVotes;
+}
